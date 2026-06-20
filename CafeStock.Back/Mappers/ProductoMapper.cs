495c79d92f631332d1a1e4f97228f076ac5837aa
Cristian <Crisalvfer14@gmail.com>
2026-06-20 12:49:37 +0200

    Añadido Entity y Mapper

---
+++ CafeStock.Back/Mappers/ProductoMapper.cs
@@ -0,0 +1,28 @@
+﻿using CafeStock.Back.Entity;
+using CafeStock.Back.Models;
+namespace CafeStock.Back.Mappers;
+
+public static class ProductoMapper
+{
+    public static ProductoEntity ToEntity(this Producto producto)
+    {
+       return  new ProductoEntity
+       {
+           Id = producto.Id,
+           Nombre = producto.Nombre,
+           StockActual = producto.StockActual,
+           StockMaximo = producto.StockMaximo,
+       };
+    }
+
+    public static Producto ToProducto(this ProductoEntity entity)
+    {
+        return new Producto
+        {
+            Id = entity.Id,
+            Nombre = entity.Nombre,
+            StockActual = entity.StockActual,
+            StockMaximo = entity.StockMaximo
+        };
+    }
+}
