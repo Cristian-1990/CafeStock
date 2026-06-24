e164146cf03fdf85f14c618857d2219bd61383fb
Cristian <Crisalvfer14@gmail.com>
2026-06-24 21:26:03 +0200

    Añadido ListaCompra

--- CafeStock.Blazor/Program.cs
+++ CafeStock.Blazor/Program.cs
@@ -1,3 +1,6 @@
+using CafeStock.Back.Infrastructure;
+using CafeStock.Back.Repositories.Productos.Base;
+using CafeStock.Back.Services.Productos;
 using CafeStock.Blazor.Components;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,21 +9,28 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+// Configurar la base de datos
+var dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "cafestock.db");
+var connectionString = $"Data Source={dbPath}";
+
+// Registrar dependencias del Back
+var provider = DependenciesProvider.BuildServiceProvider(connectionString);
+builder.Services.AddSingleton(provider.GetService<IProductoRepository>()!);
+builder.Services.AddScoped<IProductoService, CafeStock.Back.Services.Productos.ProductoService>();
+builder.Services.AddScoped<CafeStock.Back.Validators.Common.IValidador<CafeStock.Back.Models.Producto>, CafeStock.Back.Validators.Productos.ValidadorProducto>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
-
 app.UseAntiforgery();
-
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
